54e6e025800731d5d058e600d262a1a530d40f5c
art772 <art772@wp.pl>
2023-08-04 23:24:16 +0200

    Connected Angular with Web Api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'RealEstate.Angular';
+
+  estates: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:5186/api/estates/GetEstates').subscribe({
+      next: response => this.estates = response,
+      error: error => console.log(error),
+      complete: () => console.log('Request has completetd')
+    });
+  }
+
 }

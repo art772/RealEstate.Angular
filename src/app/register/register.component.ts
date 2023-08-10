9bc0e354674528b87be54dcb4fc03215bff20810
art772 <art772@wp.pl>
2023-08-10 21:36:17 +0200

    Added home page (placeholder) and register page

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  model: any = {}
+
+  constructor() {}
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  register() {
+    console.log(this.model);
+  }
+
+  cancel() {
+    console.log("Canceled");
+  }
+
+}

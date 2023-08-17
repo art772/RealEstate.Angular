3251fd0365c428986eef9a7e24e2697888f67c7c
art772 <art772@wp.pl>
2023-08-17 23:28:04 +0200

    added authGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { EstateListComponent } from './estates/estate-list/estate-list.component';
 import { EstateDetailComponent } from './estates/estate-detail/estate-detail.component';
+import { authGuard } from './_guards/auth.guard';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'estates', component: EstateListComponent},
+  {path: 'estates', component: EstateListComponent, canActivate: [authGuard]},
   {path: 'estates/:id', component: EstateDetailComponent},
   {path: '**', component: HomeComponent, },
 ];

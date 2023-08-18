841a4836fcbf4a96639128e88032f1c40e58ad63
art772 <art772@wp.pl>
2023-08-18 19:55:29 +0200

    added new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,11 @@ import { RegisterComponent } from './register/register.component';
 import { EstateListComponent } from './estates/estate-list/estate-list.component';
 import { EstateDetailComponent } from './estates/estate-detail/estate-detail.component';
 import { SharedModule } from './_modules/shared.module';
+import { CategoriesComponent } from './categories/categories.component';
+import { StatesComponent } from './states/states.component';
+import { GenresComponent } from './genres/genres.component';
+import { TagsComponent } from './tags/tags.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +25,12 @@ import { SharedModule } from './_modules/shared.module';
     HomeComponent,
     RegisterComponent,
     EstateListComponent,
-    EstateDetailComponent
+    EstateDetailComponent,
+    CategoriesComponent,
+    StatesComponent,
+    GenresComponent,
+    TagsComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
